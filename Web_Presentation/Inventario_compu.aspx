719dce946dd2c99d62b33a32ec90d415324def63
Yuliett-Rojas <yuli_hernandez_10@hotmail.com>
2022-07-26 12:02:00 -0500

    Master page pagina principal

--- Web_Presentation/Inventario_compu.aspx
+++ Web_Presentation/Inventario_compu.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master_page/inventario.Master" AutoEventWireup="true" CodeBehind="Inventario_compu.aspx.cs" Inherits="Web_Presentation.Inventario_compu" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/views/inventario.Master" AutoEventWireup="true" CodeBehind="Inventario_compu.aspx.cs" Inherits="Web_Presentation.Inventario_compu" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
--- Web_Presentation/Pruebas.aspx.cs
+++ Web_Presentation/Pruebas.aspx.cs
@@ -16,10 +16,7 @@ namespace Web_Presentation
         Clase_Negocios bl = new Clase_Negocios(System.Configuration.ConfigurationManager.ConnectionStrings["Sql_Server2"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["bl"] != null) //nulo
-            {
-                bl = (Clase_Negocios)Session["bl"]; //insertamos el campo LON
-            }
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -60,23 +57,6 @@ namespace Web_Presentation
                 Label2.Text = mensaje;
             }
 
-        }
-
-        protected void Button2_Click(object sender, EventArgs e)
-        {
-            string mensaje;
-            mensaje = bl.InsertarMouse(new Entities.Mouse()//insertamos la clase entidades
-            {
-                Id_mouse = Convert.ToInt32(id_mouse.Value),
-               f_marcamouse = Convert.ToInt32(marcamouse.Value),
-                conector = conector.Value,
-            });
-
-            Label1.Text = mensaje;
-            Session["bl"] = bl;
-            id_mouse.Value = "";
-            marcamouse.Value = "";
-            conector.Value = "";
         }
     }
 }
